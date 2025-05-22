9b60fc33d9002072bf71075b2816fb7a2c719808
Tao Su <96sutao@gmail.com>
2025-05-21 21:10:12 -0400

    reade and write file in node js

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// const hello = 'Hello world!';
+// console.log(hello);
+const textIn= fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about avocado: ${textIn}. \n Create on ${Date.now()}\n`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File is written successfully.');
