59e25c3d87efa34d93eb8c248fdd0909806f8161
Tao Su <96sutao@gmail.com>
2025-05-21 23:23:26 -0400

    reade and write file in node js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,60 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
 // const hello = 'Hello world!';
 // console.log(hello);
-const textIn= fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
 
-const textOut = `This is what we know about avocado: ${textIn}. \n Create on ${Date.now()}\n`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File is written successfully.');
+///////////////////////
+// files
+
+// Blocking , synchronous way
+// const textIn= fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+//
+// const textOut = `This is what we know about avocado: ${textIn}. \n Create on ${Date.now()}\n`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File is written successfully.');
+
+// non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8',(err, data1) => {
+//     if (err) throw err;
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8',(err, data2) => {
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`, 'utf-8',(err, data3) => {
+//             console.log(data3);
+//             fs.writeFile(`./txt/final.txt`,`${data2}\n${data3}`, `utf-8`,(err) => {
+//                 console.log("File has been written.")
+//             })
+//         })
+//     })
+//     // console.log(data1);
+//     // if (err) throw err;
+// })
+
+// console.log('It will read the file.');
+
+// Server
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    const pathName = req.url;
+    if(pathName === '/overview' || pathName === '/') {
+        res.end('This is the overview page!');
+    } else if (pathName === '/product') {
+        res.end('This is the product page!');
+    } else {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'my-own-head' : 'hello world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Server started and listening on port 8080');
+});
+
+
